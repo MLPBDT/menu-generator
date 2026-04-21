34b53922a1542bd582a4b108db0154e56fe0ebeb
MLPBDT <108140136+MLPBDT@users.noreply.github.com>
2026-04-21 15:03:10 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         model: "llama-3.3-70b-versatile",
-        max_tokens: 1500,
+        max_tokens: 2000,
         messages: messages
       })
     });
